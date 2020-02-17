56ef9113a026ffbc8ad65badad7041110ddbf80e
inam <inam.haq@attech-ltd.com>
2020-02-17 12:05:04 +0000

    first commit

---
+++ app/User.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App;
+
+
+use Illuminate\Notifications\Notifiable;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+class User extends Authenticatable
+{
+    use  Notifiable;
+    use HasRoles;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
